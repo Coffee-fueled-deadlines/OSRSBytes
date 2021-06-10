bbc875087a131938585ce4e8f4336b386dd41d95
Tyler Sweat <tysweat0@gmail.com>
2021-06-10 18:05:28 +0000

    Fixed bug in setup.py causing a manual install to fail and matched version numbers to 1.2.4 (past 1.2.3 on PyPI)

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='OSRSBytes',  
-    version='1.2.0',
+    version='1.2.4',
     author="Coffee Fueled Deadlines",
     author_email="cookm0803@gmail.com",
     description="An all-in-one OSRS Library with Hiscores and Grand Exchange Market Information",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Coffee-fueled-deadlines/OSRSBytes",
+    packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
