96996057341495bbcb10c26007f8170a94e00c63
CFDeadlines <47742787+Coffee-fueled-deadlines@users.noreply.github.com>
2022-11-30 15:13:23 -0500

    Added setup functionality for including modules

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Coffee-fueled-deadlines/OSRSBytes",
     packages=setuptools.find_packages(),
+    # package_data={"OSRSBytes": ["modules/*"]}, # This will be included soon
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
