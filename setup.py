b4500a32188c9b8eb0fbf4b1cc23f346cb5649d8
CFDeadlines <47742787+Coffee-fueled-deadlines@users.noreply.github.com>
2022-12-05 14:27:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='OSRSBytes',  
-    version='1.2.3',
+    version='1.3.0',
     author="Coffee Fueled Deadlines",
     author_email="cookm0803@gmail.com",
     description="An all-in-one OSRS Library with Hiscores and Grand Exchange Market Information",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Coffee-fueled-deadlines/OSRSBytes",
-    packages=setuptools.find_packages() + ['modules'],
-    include_package_data=True,
     packages=setuptools.find_packages(),
+    # package_data={"OSRSBytes": ["modules/*"]}, # This will be included soon
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: EPL-2.0 License",
+        "License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)",
         "Operating System :: OS Independent",
     ],
 )
